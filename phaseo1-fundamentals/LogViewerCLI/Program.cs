b6a7bb7d0b3343047c7d1ea045d95234c4f00c43
Maxuel Barbosa Rodrigues <maxuel.rodrigues@sefaz.es.gov.br>
2026-08-07 14:31:58 -0300

    feat(day-01): setup dotnet sdk, cli commands and top-level statements

---
+++ phaseo1-fundamentals/LogViewerCLI/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("==========================================");
+Console.WriteLine(" 🚀 LogViewer CLI - Day 01 Started");
+Console.WriteLine("==========================================");
+
+if (args.Length > 0)
+{
+    Console.WriteLine($"\n[ARGUMENTOS RECEBIDOS]: {string.Join(", ", args)}");
+}
+else
+{
+    Console.WriteLine($"\n[INFO]: Nenhum argumento fornecido via CLI.");
+}
+
+Console.WriteLine("\nAmbiente configurado com sucesso! Pronto para o Dia 02.");
